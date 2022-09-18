714f671d48344eb33e1be5a4a18514bd7b0f805d
guga esitashvili <gesitashvili5591@sdsu.edu>
2022-09-17 18:20:02 -0700

    robot's location is done, next thing is implementing sonar system for detecting obstacles

--- robot.js
+++ robot.js
@@ -4,18 +4,21 @@ class Robot {
     matrix = null;
     robotMap = null;
     obstaclePercentage = null;
+    repeatedLocations = [[7, 5], [9, 9]];
+    robotOrientation = 0;
+
     constructor(matrixWidth, matrixHeight, obstaclePercentage){
         this.matrixWidth = matrixWidth;
         this.matrixHeight = matrixHeight;
         this.obstaclePercentage = obstaclePercentage;
         /* matrix initialization with desired dimensions and initial values */
         this.matrix = Array(matrixHeight).fill(null).map(() => Array(matrixWidth).fill(2)); 
-        this.generateObstacles(obstaclePercentage);
         this.matrix[9][9] = 'D' /* initializing destination */
-        this.robotMap = this.matrix;
-        console.table(this.matrix)
+        this.robotMap = this.matrix.map(a => a.slice())
+        this.generateObstacles(obstaclePercentage);
         this.robotMap[7][4] = "S" /* initializing robot */
-        console.table(this.robotMap)
+        //console.table(this.matrix);
+        console.table(this.robotMap);
     }
 
     generateObstacles(obstaclePercentage){
@@ -30,7 +33,6 @@ class Robot {
         }
     }
 
-    repeatedLocations = [[7, 5], [9, 9]]
     generateRandomNumber(maxWidth, maxHeight){
         
         const xRand = Math.floor(Math.random() * (maxWidth) + 0)
@@ -46,11 +48,97 @@ class Robot {
         this.repeatedLocations.push([xRand, yRand]);
         return [xRand, yRand];
     }
+    getRobotLocation(){
+        for(var i = 0; i < this.robotMap.length; i++) {
+            for(var j = 0; j < this.robotMap[i].length; j++) {
+                if(this.robotMap[i][j] == 'S'){
+                    return [i, j];
+                }
+            }
+        }
+    }
+
+    isThereObstacle(x, y){
+        if(this.matrix[x][y] == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    useSonar() {
+        var view = this.getRobotLocation() 
+        /* variable to getting robot's current 
+        location and then checking different ones */
+
+        switch(this.robotOrientation){
+            case 0:
+                console.log(this.robotMap[view[0]][view[1]]);
+                while(this.robotMap[view[0]][view[1]] != 0)
+                break;
+            case 45:
+                this.robotMap[robotLocation[0] - 1][robotLocation[1] + 1] = 'S';
+                break;
+            case 90:
+                this.robotMap[robotLocation[0]][robotLocation[1] + 1] = 'S';
+                break;
+            case 135:
+                this.robotMap[robotLocation[0] + 1][robotLocation[1] + 1] = 'S';
+                break;
+            case 180:
+                this.robotMap[robotLocation[0] + 1][robotLocation[1]] = 'S';
+                break;
+            case 225:
+                this.robotMap[robotLocation[0] + 1][robotLocation[1] -1] = 'S';
+                break;
+            case 270:
+                this.robotMap[robotLocation[0]][robotLocation[1] - 1] = 'S';
+                break;
+            case 315:
+                this.robotMap[robotLocation[0] - 1][robotLocation[1] - 1] = 'S';
+                break;
+        }
+    }
+
+    goForward(){
+        var robotLocation = this.getRobotLocation()
+        this.robotMap[robotLocation[0]][robotLocation[1]] = 1;
+
+        switch(this.robotOrientation){
+            case 0:
+                this.robotMap[robotLocation[0] - 1][robotLocation[1]] = 'S';
+                break;
+            case 45:
+                this.robotMap[robotLocation[0] - 1][robotLocation[1] + 1] = 'S';
+                break;
+            case 90:
+                this.robotMap[robotLocation[0]][robotLocation[1] + 1] = 'S';
+                break;
+            case 135:
+                this.robotMap[robotLocation[0] + 1][robotLocation[1] + 1] = 'S';
+                break;
+            case 180:
+                this.robotMap[robotLocation[0] + 1][robotLocation[1]] = 'S';
+                break;
+            case 225:
+                this.robotMap[robotLocation[0] + 1][robotLocation[1] -1] = 'S';
+                break;
+            case 270:
+                this.robotMap[robotLocation[0]][robotLocation[1] - 1] = 'S';
+                break;
+            case 315:
+                this.robotMap[robotLocation[0] - 1][robotLocation[1] - 1] = 'S';
+                break;
+        }
+        console.table(this.robotMap);
+    }
+
+    start() {
+        this.useSonar()
+    }
     out() {
-        console.table(this.matrix);
+        // console.table(this.matrix);
     }
 }
 
 const myRobot = new Robot(10, 10, 10);
-
-array = myRobot.generateRandomNumber(40);
+myRobot.start()
